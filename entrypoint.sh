405cc46b4dad415698d1053acb1c307113a15204
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-08-27 22:52:25 +0800

    fix: entrypoint typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 ROOT_DIR=/usr/share/nginx/html
 VITE_API_URL=${VITE_API_URL:-"http://localhost:8080/api/cheese-auth"}
 VITE_SANDBOX_URL=${VITE_SANDBOX_URL:-"http://localhost:8080/api/sandbox"}
-VITE_SANDBOX_URL=${ENABLE_USERNAME_LOGIN:-"true"}
+ENABLE_USERNAME_LOGIN=${ENABLE_USERNAME_LOGIN:-"true"}
 
 echo "window.runtimeConfig = { VITE_API_URL: '${VITE_API_URL}', VITE_SANDBOX_URL: '${VITE_SANDBOX_URL}', ENABLE_USERNAME_LOGIN: ${ENABLE_USERNAME_LOGIN} };" > ${ROOT_DIR}/config.js
 echo "Generated ${ROOT_DIR}/config.js with API URL: ${VITE_API_URL}"
